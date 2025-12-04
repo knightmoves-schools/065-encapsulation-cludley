b1a7b714240994ccbff52d0a086ba170f365e1c7
cludley <cludley@anamosa.k12.ia.us>
2025-12-04 18:59:12 +0000

    n

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 
-let length = 5;
-let height = 10;
+class Rectangle {
+    constructor(length, height) {
+        this.length = length;
+        this.height = height;
+    }
 
-function calculateArea(length, height){
-    return length * height;
+    calculateArea() {
+        return this.length * this.height;
+    }
 }
+
